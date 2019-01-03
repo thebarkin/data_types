e3c3f4a73ffbfd94476a9ba6e23777ccace6c034
Richard Barkinskiy <richard.barkinskiy@superleague.com>
2019-01-03 10:28:49 -0800

    initial commit

---
+++ song.js
@@ -0,0 +1,35 @@
+/** List of Song Attributes
+ *  for a Nine Inch Nails Song
+ *  called With  Teeth
+ */
+
+//Strings
+const artist = "Nine Inch Nails";
+const album = "With Teeth";
+const label = "Interscope";
+const genre = "Industrial";
+
+//Objects
+const nominations = {
+  grammy: "Award for Best Hard Rock Performance",
+  billboardMusic: "Award for Top Rock Song",
+};
+const origin = {
+  country: "United States",
+  state: "Ohio",
+  city: "Cleveland",
+}
+
+//Arrays
+const currentBandMembers = ["Trent Reznor", "Atticus Ross"];
+const pastBandMembers = ["Robin Finck", "Alessandro Cortini", "Ilan Rubin"];
+
+//Numbers
+const songedReleaseDate = 2005;
+const currentMemberCount = 2;
+
+//Boolean
+const bandOnTour = true;
+
+//Log out attributes
+console.log(artist, album, songedReleaseDate, label, genre, nominations, currentBandMembers, pastBandMembers, bandOnTour, origin);
